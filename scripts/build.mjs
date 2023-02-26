a54794ff4b518d2d2a4ffba60bdaf316f4fe7aaf
Mark Martirosian <hello@markmartirosian.com>
2023-02-26 16:04:57 +0100

    fix: build scripts
    Closes #58

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,30 +1,13 @@
 import { build } from 'esbuild'
 import { execa } from 'execa'
 import fse from 'fs-extra'
-import { mkdir } from 'fs/promises'
 import path from 'path'
 import process from 'process'
-import { cwd, external, version, name } from './constants.mjs'
+import { cwd, external, name, version } from './constants.mjs'
 
 const tsconfig = fse.existsSync(path.join(cwd, 'tsconfig-build.json'))
   ? path.join(cwd, 'tsconfig-build.json')
   : path.join(cwd, 'tsconfig.json')
-
-const getOptions = (format) => ({
-  format,
-  outdir: path.join(cwd, `lib/${format}`),
-  tsconfig,
-  entryPoints:
-    name === 'cli'
-      ? ['src/index.ts', 'src/worker.ts', 'src/cli.ts']
-      : ['src/index.ts'],
-  splitting: name === 'cli'
-  /* name === 'yeux' */
-  /*   ? ['lib/tsc/index.js', 'lib/tsc/cli.js'] */
-  /*   : ['src/index.ts'] */
-})
-
-const formats = ['esm']
 
 process.umask(0o022)
 process.chdir(cwd)
@@ -37,7 +20,9 @@ await execa(
     '-p',
     path.relative(cwd, tsconfig),
     '--declaration',
-    '--emitDeclarationOnly'
+    '--emitDeclarationOnly',
+    '--declarationDir',
+    'lib/types'
   ],
   {
     all: true,
@@ -48,31 +33,26 @@ await execa(
   process.exit(reason.exitCode)
 })
 
-await Promise.all(
-  formats.map(async (format) => {
-    const options = getOptions(format)
-    await fse.remove(options.outdir)
-    await mkdir(options.outdir, { recursive: true })
+const buildOptions = {
+  bundle: true,
+  define: {
+    __VERSION__: JSON.stringify(version)
+  },
+  mainFields: ['module'],
+  entryPoints: ['src/index.ts'],
+  external: ['cassiopeia', '@cassiopeia/*', 'vue', ...external],
+  format: 'esm',
+  logLevel: 'info',
+  outExtension: { '.js': '.mjs' },
+  outdir: path.join(cwd, `lib/esm`),
+  platform: 'neutral',
+  sourcemap: true,
+  splitting: true,
+  tsconfig
+}
 
-    await build({
-      bundle: true,
-      external: [
-        'cepheus',
-        '@cepheus/*',
-        'cassiopeia',
-        '@cassiopeia/*',
-        'vue',
-        '@vue/*',
-        ...external
-      ],
-      define: {
-        VERSION: JSON.stringify(version)
-      },
-      logLevel: 'info',
-      outExtension: { '.js': `.${options.format === 'esm' ? 'mjs' : 'cjs'}` },
-      platform: 'node',
-      sourcemap: true,
-      ...options
-    })
-  })
-)
+if (name === '@cepheus/cli') {
+  await build({ ...buildOptions, platform: 'node' })
+} else {
+  await build(buildOptions)
+}
