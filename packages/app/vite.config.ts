1f8085d1a5fb5a4fcfd2ae4efa004061676a2ded
yyxi <178086254+yyxi@users.noreply.github.com>
2023-03-21 21:24:11 +0100

    fix: public dir

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,8 +1,7 @@
 import { cassiopeia } from '@cassiopeia/vite'
 import vue from '@vitejs/plugin-vue'
 import unocss from 'unocss/vite'
-import { defineConfig } from 'vite'
-import { splitVendorChunkPlugin } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 
 export default defineConfig({
   plugins: [
