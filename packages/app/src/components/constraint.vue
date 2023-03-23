3d7bef3f456723f013652d421da1571c8f47187c
Mark Martirosian <hello@markmartirosian.com>
2023-03-24 00:47:20 +0100

    fix: small refactor

--- packages/app/src/components/constraint.vue
+++ packages/app/src/components/constraint.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { INTERPOLATOR, type Triangle } from 'cepheus'
-import { onMounted } from 'vue'
+import { onMounted, onUnmounted } from 'vue'
 // import { convert, OKLCH, P3, sRGB } from '@cepheus/color'
 import { useCepheus } from '@cepheus/vue'
-import { useCepheusStore } from '../use-cepheus-store'
+import { useCepheusStore } from '../hooks/use-cepheus-store'
 import { usePane } from '../hooks/use-pane'
 
 const instance = useCepheus()
@@ -12,6 +12,87 @@ const model = instance[INTERPOLATOR].state.model
 
 // function cross(a: Point, b: Point, c: Point) {
 //   return (b[0] - a[0]) * -(c[1] - a[1]) - -(b[1] - a[1]) * (c[0] - a[0])
+// }
+
+// const interpolator = (alpha: number, beta: number, gamma: number) => {
+//   const coords = barycentric(instance, 0, alpha, beta, gamma)
+//
+//   if (coords === undefined) {
+//     return [255, 255, 255]
+//   }
+//
+//   return convert(
+//     {
+//       space: OKLCH,
+//       alpha: 1,
+//       coords
+//     },
+//     supportsDisplayP3 ? P3 : sRGB,
+//     { inGamut: true }
+//   ).coords.map((value) => value * 255)
+// }
+
+// function fillTriangle(imageData: ImageData, triangle: Triangle) {
+//   const v0 = triangle[0]
+//   const v1 = triangle[1]
+//   const v2 = triangle[2]
+//
+//   const minX = Math.floor(Math.min(v0[0], v1[0], v2[0]))
+//   const maxX = Math.ceil(Math.max(v0[0], v1[0], v2[0]))
+//   const minY = Math.floor(Math.min(v0[1], v1[1], v2[1]))
+//   const maxY = Math.ceil(Math.max(v0[1], v1[1], v2[1]))
+//
+//   const data = imageData.data
+//   const width = imageData.width
+//
+//   // precalculate the area of the parallelogram defined by our triangle
+//   const area = cross(v0, v1, v2)
+//
+//   // get all properties on our first vertex, for interpolating later
+//   // const props = Object.getOwnPropertyNames(v0)
+//
+//   // p is our 2D pixel location point
+//   // const p: Point = {}
+//
+//   // fragment is the resulting pixel with all the vertex attributes interpolated
+//   // const fragment = {}
+//
+//   for (let y = minY; y < maxY; y++) {
+//     for (let x = minX; x < maxX; x++) {
+//       if (x % 2 === 0 || y % 2 === 0) {
+//         continue
+//       }
+//
+//       // sample from the center of the pixel, not the top-left corner
+//       const p: Point = [x + 0.5, y + 0.5]
+//
+//       // calculate vertex weights
+//       // should divide these by area, but we do that later
+//       // so we divide once, not three times
+//       const w0 = cross(v1, v2, p) / area
+//       const w1 = cross(v2, v0, p) / area
+//       const w2 = cross(v0, v1, p) / area
+//
+//       // const [w0, w1, w2] = cartesianToBarycentric(p, v0, v1, v2)
+//
+//       if (Math.max(w0, Math.max(w1, w2)) > 1.0) {
+//         continue
+//       }
+//
+//       if (Math.min(w0, Math.min(w1, w2)) < 0.0) {
+//         continue
+//       }
+//
+//       const [R, G, B] = interpolator(w0, w1, w2)
+//
+//       const index = (y * width + x) * 4
+//
+//       data[index + 0] = R
+//       data[index + 1] = G
+//       data[index + 2] = B
+//       data[index + 3] = 255
+//     }
+//   }
 // }
 
 usePane()
@@ -39,93 +120,14 @@ onMounted(() => {
     context = canvas.getContext('2d', { colorSpace: 'srgb' })!
   }
 
-  // const interpolator = (alpha: number, beta: number, gamma: number) => {
-  //   const coords = barycentric(instance, 0, alpha, beta, gamma)
-  //
-  //   if (coords === undefined) {
-  //     return [255, 255, 255]
-  //   }
-  //
-  //   return convert(
-  //     {
-  //       space: OKLCH,
-  //       alpha: 1,
-  //       coords
-  //     },
-  //     supportsDisplayP3 ? P3 : sRGB,
-  //     { inGamut: true }
-  //   ).coords.map((value) => value * 255)
-  // }
-
-  // function fillTriangle(imageData: ImageData, triangle: Triangle) {
-  //   const v0 = triangle[0]
-  //   const v1 = triangle[1]
-  //   const v2 = triangle[2]
-  //
-  //   const minX = Math.floor(Math.min(v0[0], v1[0], v2[0]))
-  //   const maxX = Math.ceil(Math.max(v0[0], v1[0], v2[0]))
-  //   const minY = Math.floor(Math.min(v0[1], v1[1], v2[1]))
-  //   const maxY = Math.ceil(Math.max(v0[1], v1[1], v2[1]))
-  //
-  //   const data = imageData.data
-  //   const width = imageData.width
-  //
-  //   // precalculate the area of the parallelogram defined by our triangle
-  //   const area = cross(v0, v1, v2)
-  //
-  //   // get all properties on our first vertex, for interpolating later
-  //   // const props = Object.getOwnPropertyNames(v0)
-  //
-  //   // p is our 2D pixel location point
-  //   // const p: Point = {}
-  //
-  //   // fragment is the resulting pixel with all the vertex attributes interpolated
-  //   // const fragment = {}
-  //
-  //   for (let y = minY; y < maxY; y++) {
-  //     for (let x = minX; x < maxX; x++) {
-  //       if (x % 2 === 0 || y % 2 === 0) {
-  //         continue
-  //       }
-  //
-  //       // sample from the center of the pixel, not the top-left corner
-  //       const p: Point = [x + 0.5, y + 0.5]
-  //
-  //       // calculate vertex weights
-  //       // should divide these by area, but we do that later
-  //       // so we divide once, not three times
-  //       const w0 = cross(v1, v2, p) / area
-  //       const w1 = cross(v2, v0, p) / area
-  //       const w2 = cross(v0, v1, p) / area
-  //
-  //       // const [w0, w1, w2] = cartesianToBarycentric(p, v0, v1, v2)
-  //
-  //       if (Math.max(w0, Math.max(w1, w2)) > 1.0) {
-  //         continue
-  //       }
-  //
-  //       if (Math.min(w0, Math.min(w1, w2)) < 0.0) {
-  //         continue
-  //       }
-  //
-  //       const [R, G, B] = interpolator(w0, w1, w2)
-  //
-  //       const index = (y * width + x) * 4
-  //
-  //       data[index + 0] = R
-  //       data[index + 1] = G
-  //       data[index + 2] = B
-  //       data[index + 3] = 255
-  //     }
-  //   }
-  // }
-
   const toX = (x: number) => Math.floor((x / 240) * canvas.width)
   const toY = (y: number) => Math.floor((y / 240) * canvas.height)
 
+  const store = useCepheusStore()
+
   const strokeTriangle = (triangle: Triangle) => {
     context!.lineWidth = 2
-    context!.strokeStyle = 'black'
+    context!.strokeStyle = store.darkMode === true ? 'white' : 'black'
     context!.beginPath()
     context!.moveTo(triangle[0][0], triangle[0][1])
     context!.lineTo(triangle[1][0], triangle[1][1])
@@ -134,17 +136,14 @@ onMounted(() => {
     context!.stroke()
   }
 
-  const store = useCepheusStore()
-
-  store.$subscribe(() => {
+  const update = () => {
     requestAnimationFrame(() => {
       const img = context!.createImageData(
         context!.canvas.width,
         context!.canvas.height,
         { colorSpace: supportsDisplayP3 ? 'display-p3' : 'srgb' }
       )
-      // setChroma(instance, 0, 1)
-      // setLightness(instance, 0, 1)
+
       context!.clearRect(0, 0, canvas.width, canvas.height)
 
       const modelTriangle = model.triangle.map(([x, y]) => [
@@ -161,7 +160,11 @@ onMounted(() => {
       strokeTriangle(modelTriangle)
       strokeTriangle(triangle)
     })
-  })
+  }
+
+  onUnmounted(store.$subscribe(update))
+
+  update()
 })
 </script>
 
